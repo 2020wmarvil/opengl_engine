7598a5c7a50e856580c5602a1a90adfc9b3d6fa0
Wyatt Marvil <2020wmarvil@tjhsst.edu>
2019-07-14 17:06:04 -0400

    remove useless scripty

--- clear.sh
+++
@@ -1,4 +0,0 @@
-rm -rf CMakeCache.txt CMakeFiles Makefile cmake_install.cmake
-rm -rf bin/cmake_install.cmake bin/CMakeCache.txt bin/CMakeFiles bin/Makefile bin/cmake_install.cmake
-rm -rf src/cmake_install.cmake src/CMakeCache.txt src/CMakeFiles src/Makefile src/cmake_install.cmake
-clear
